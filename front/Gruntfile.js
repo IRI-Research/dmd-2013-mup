6226e33a87adb89d74a8095038a74bf1cf4cf0fb
La rosa kévin <larosa.kevin@gmail.com>
2013-05-12 21:06:59 +0200

    implementation widget

--- front/Gruntfile.js
+++ front/Gruntfile.js
@@ -1,4 +1,4 @@
-// Generated on 2013-05-10 using generator-webapp 0.1.7
+// Generated on 2013-04-05 using generator-webapp 0.1.5
 'use strict';
 var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
 var mountFolder = function (connect, dir) {
@@ -34,21 +34,21 @@ module.exports = function (grunt) {
             },
             compass: {
                 files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
-                tasks: ['compass:server']
+                tasks: ['compass']
             },
             livereload: {
                 files: [
                     '<%= yeoman.app %>/*.html',
                     '{.tmp,<%= yeoman.app %>}/styles/{,*/}*.css',
                     '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
-                    '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                    '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,webp,gif}'
                 ],
                 tasks: ['livereload']
             }
         },
         connect: {
             options: {
-                port: 9000,
+                port: 8000,
                 // change this to '0.0.0.0' to access the server from outside
                 hostname: 'localhost'
             },
@@ -89,16 +89,7 @@ module.exports = function (grunt) {
             }
         },
         clean: {
-            dist: {
-                files: [{
-                    dot: true,
-                    src: [
-                        '.tmp',
-                        '<%= yeoman.dist %>/*',
-                        '!<%= yeoman.dist %>/.git*'
-                    ]
-                }]
-            },
+            dist: ['.tmp', '<%= yeoman.dist %>/*'],
             server: '.tmp'
         },
         jshint: {
@@ -123,9 +114,11 @@ module.exports = function (grunt) {
         coffee: {
             dist: {
                 files: [{
+                    // rather than compiling multiple files here you should
+                    // require them into your main .coffee file
                     expand: true,
                     cwd: '<%= yeoman.app %>/scripts',
-                    src: '{,*/}*.coffee',
+                    src: '*.coffee',
                     dest: '.tmp/scripts',
                     ext: '.js'
                 }]
@@ -133,10 +126,9 @@ module.exports = function (grunt) {
             test: {
                 files: [{
                     expand: true,
-                    cwd: 'test/spec',
-                    src: '{,*/}*.coffee',
-                    dest: '.tmp/spec',
-                    ext: '.js'
+                    cwd: '.tmp/spec',
+                    src: '*.coffee',
+                    dest: 'test/spec'
                 }]
             }
         },
@@ -162,21 +154,22 @@ module.exports = function (grunt) {
         /*concat: {
             dist: {}
         },*/
-        // not enabled since usemin task does concat and uglify
-        // check index.html to edit your build targets
-        // enable this task if you prefer defining your build targets here
-        /*uglify: {
-            dist: {}
-        },*/
-        rev: {
+        requirejs: {
             dist: {
-                files: {
-                    src: [
-                        '<%= yeoman.dist %>/scripts/{,*/}*.js',
-                        '<%= yeoman.dist %>/styles/{,*/}*.css',
-                        '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
-                        '<%= yeoman.dist %>/styles/fonts/*'
-                    ]
+                // Options: https://github.com/jrburke/r.js/blob/master/build/example.build.js
+                options: {
+                    // `name` and `out` is set by grunt-usemin
+                    baseUrl: 'app/scripts',
+                    optimize: 'none',
+                    // TODO: Figure out how to make sourcemaps work with grunt-usemin
+                    // https://github.com/yeoman/grunt-usemin/issues/30
+                    //generateSourceMaps: true,
+                    // required to support SourceMaps
+                    // http://requirejs.org/docs/errors.html#sourcemapcomments
+                    preserveLicenseComments: false,
+                    useStrict: true,
+                    wrap: true,
+                    //uglify2: {} // https://github.com/mishoo/UglifyJS2
                 }
             }
         },
@@ -198,17 +191,7 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     cwd: '<%= yeoman.app %>/images',
-                    src: '{,*/}*.{png,jpg,jpeg}',
-                    dest: '<%= yeoman.dist %>/images'
-                }]
-            }
-        },
-        svgmin: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= yeoman.app %>/images',
-                    src: '{,*/}*.svg',
+                    src: '{,*/}*.{png,jpg,jpeg,gif}',
                     dest: '<%= yeoman.dist %>/images'
                 }]
             }
@@ -244,7 +227,6 @@ module.exports = function (grunt) {
                 }]
             }
         },
-        // Put files not handled in other tasks here
         copy: {
             dist: {
                 files: [{
@@ -254,29 +236,15 @@ module.exports = function (grunt) {
                     dest: '<%= yeoman.dist %>',
                     src: [
                         '*.{ico,txt}',
-                        '.htaccess',
-                        'images/{,*/}*.{webp,gif}',
-                        'styles/fonts/*'
+                        '.htaccess'
                     ]
                 }]
             }
         },
-        concurrent: {
-            server: [
-                'coffee:dist',
-                'compass:server'
-            ],
-            test: [
-                'coffee',
-                'compass'
-            ],
-            dist: [
-                'coffee',
-                'compass:dist',
-                'imagemin',
-                'svgmin',
-                'htmlmin'
-            ]
+        bower: {
+            all: {
+                rjsConfig: '<%= yeoman.app %>/scripts/main.js'
+            }
         }
     });
 
@@ -289,7 +257,8 @@ module.exports = function (grunt) {
 
         grunt.task.run([
             'clean:server',
-            'concurrent:server',
+            'coffee:dist',
+            'compass:server',
             'livereload-start',
             'connect:livereload',
             'open',
@@ -299,20 +268,24 @@ module.exports = function (grunt) {
 
     grunt.registerTask('test', [
         'clean:server',
-        'concurrent:test',
+        'coffee',
+        'compass',
         'connect:test',
         'mocha'
     ]);
 
     grunt.registerTask('build', [
         'clean:dist',
+        'coffee',
+        'compass:dist',
         'useminPrepare',
-        'concurrent:dist',
-        'cssmin',
+        'requirejs',
+        'imagemin',
+        'htmlmin',
         'concat',
+        'cssmin',
         'uglify',
         'copy',
-        'rev',
         'usemin'
     ]);
 
--- front/app/scripts/main.js
+++ front/app/scripts/main.js
@@ -1,1 +1,33 @@
-console.log('\'Allo \'Allo!');
+require.config({
+    paths: {
+        jquery: '../components/jquery/jquery',
+        bootstrap: 'vendor/bootstrap',
+        paper: 'vendor/paper',
+        backbone: 'vendor/backbone-min',
+        lodash: 'vendor/lodash.min',
+        text: 'vendor/text',
+        jqueryUI: 'vendor/jquery-ui'
+    },
+    shim: {
+        bootstrap: {
+            deps: ['jquery'],
+            exports: 'jquery'
+        },
+        paper : {
+            exports: 'Paper'
+        },
+        backbone: {
+            deps:['jquery','lodash'],
+            exports: 'Backbone'
+        },
+        jqueryUI: {
+            deps: ['jquery'],
+            exports: 'jqueryUI'
+        }
+    }
+});
+
+require(['app', 'jquery', 'bootstrap', 'paper','backbone','jqueryUI'], function (App, $,boostrap, Paper, Backbone,jqueryUI) {
+    'use strict';
+    App.initialize();
+});
